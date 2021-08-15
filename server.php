e8b827723aa9a64c1ae52b919a666870d02f054b
williamd5 <will.duncan.nn+github@gmail.com>
2021-08-15 12:39:46 +0300

    add check if file exists

--- server.php
+++ server.php
@@ -9,5 +9,8 @@
  */
 $mcfg = json_decode(file_get_contents(__DIR__ . "/../../private/backend/json/config.json"));
 $path = __DIR__ . "/.." . $_SERVER['REQUEST_URI'] . ".php";
-header("Content-type: " . mime_content_type($path));
-include($path);
+if (file_exists($path)) {
+    header("Content-type: " . mime_content_type($path));
+    include($path);
+}
+else header($_SERVER['SERVER_PROTOCOL'] . " 404");
