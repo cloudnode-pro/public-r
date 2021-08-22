1c8b42cad452eb06bcdf87ab83d44f59b371a499
williamd5 <will.duncan.nn+github@gmail.com>
2021-08-23 02:17:29 +0300

    remove some of the debugs

--- js/main.js.php
+++ js/main.js.php
@@ -216,7 +216,6 @@ const main = {
                     let code = match.substr(5, match.length - 7),
                         codeEval = null
                     try {
-                        console.log(code);
                         codeEval = eval(code);
                         codeEval === undefined ? codeEval = "" : codeEval;
                     }
