464f1fbe62e334083a3f01726d6bb438b0f643f7
williamd5 <will.duncan.nn+github@gmail.com>
2021-08-23 14:40:20 +0300

    PHP has been removed as a dependency

--- css/main.css.php
+++
@@ -1,9 +0,0 @@
-/* Copyright (C) 2021 Cloudnode - All Rights Reserved
- * Any use, distribution or modification of this code
- * is subject to the terms of the provided license.
- *
- * The license is available at the root (/) of the
- * repository. If not, please write to:
- * support@cloudnode.pro
- */
-<?= include(__DIR__ . "/_main.css"); ?>
