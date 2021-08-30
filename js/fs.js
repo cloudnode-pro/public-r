e1cb0569d8e11b3b8b43cfbd80eee56b4195e23b
williamd5 <will.duncan.nn+github@gmail.com>
2021-08-30 17:35:12 +0300

    add cloudnode fs lib

---
+++ js/fs.js
@@ -0,0 +1,81 @@
+function CdnApi (api, ep = `cdn.${location.hostname}`, enabled = true) {
+  this.api = api;
+  this.endpoint = ep;
+  this.enabled = true;
+}
+function CloudnodeFS (api) {
+  const fs = this;
+  if (api instanceof CdnApi) this.api = api;
+  this.File = function ({name, mode, created, size}) {
+    const path = name.split("/");
+    this.path = name;
+    this.basename = path.slice(path.length - 1)[0];
+    this.mode = mode;
+    this.created = new Date(created);
+    this.size = size;
+  }
+  this.FileCollection = function (files = []) {
+    if (!(files instanceof Array)) throw new Error(`@files must be Array(); ${files?.constructor?.name} given`)
+    this.length = 0;
+    this.map = {};
+    this.push = function (file, parent = null) {
+      if (file instanceof fs.File || file instanceof fs.Directory) {
+        if (parent instanceof fs.Directory) file.parent = parent;
+        this[this.length] = file;
+        this.map[file.basename] = this.length;
+        ++this.length;
+      }
+      else throw new Error(`@file must be fs.File() or fs.Directory; ${file?.constructor?.name} given`);
+    }
+    for (let file of files) if (file instanceof fs.File) this.push(file);
+    this.forEach = function (fn = new Function) {
+      for (let i in this) {
+        const file = this[i];
+        if (file instanceof fs.File) fn(file, +i, this);
+      }
+    }
+    this.get = function (basename) {
+      return this[this.map[basename]];
+    }
+  }
+  this.Directory = function ({name, collection}) {
+    if (!(collection instanceof fs.FileCollection)) throw new Error(`@files must be Array(); ${collection?.constructor?.name} given`)
+    const path = name.split("/");
+    this.path = name;
+    this.basename = path.slice(path.length - 1)[0];
+    this.files = collection;
+    this.parent = null;
+  }
+  this.link = function (files) {
+    if (files instanceof fs.File) files = new fs.FileCollection([files]);
+    else if (files instanceof Array) files = new fs.FileCollection(files);
+    if (!(files instanceof fs.FileCollection)) throw new Error(`Cannot construct fs.FileCollection() from @files. Acceptable options for @files are Array(), fs.File() and fs.FileCollection(); ${files?.constructor?.name} given`);
+    files.forEach(file => {
+      const path = file.path.split("/");
+      let parent = fs.tree;
+      for (let i in path) {
+        const segment = path[i];
+        let nDir = parent.files.get(segment);
+        if (!(nDir instanceof fs.Directory) && i < path.length - 1) {
+          const dir = fs.mkdir(parent, new fs.Directory({name: path.slice(0, i).join("/"), collection: new fs.FileCollection()}));
+          parent = dir;
+        }
+        else parent.files.push(file, parent);
+      }
+    });
+  }
+  this.mkdir = function (parent, dir) {
+    if (!(parent instanceof fs.Directory || parent instanceof fs.FileCollection)) throw new Error(`@parent fs.Directory() or fs.FileCollection(); ${parent?.constructor?.name} given`);
+    else if (!(dir instanceof fs.Directory)) throw new Error(`@dir must be fs.Directory(); ${dir?.constructor?.name} given`);
+    if (parent instanceof fs.Directory) parent = parent.files;
+    dir.parent = parent;
+    parent.push(dir);
+    return dir;
+  }
+  this.tree = new fs.Directory({name:"@root", collection: new fs.FileCollection()});
+}
+
+main.fs = {
+  driver: CloudnodeFS,
+  cdn: CdnApi
+}
