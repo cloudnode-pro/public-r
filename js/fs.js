66623cc0ee6dfd7893cd29c043c0da810e9bacdf
williamd5 <will.duncan.nn+github@gmail.com>
2021-09-03 02:24:48 +0300

    try this?

--- js/fs.js
+++ js/fs.js
@@ -61,7 +61,7 @@ function CloudnodeFS (api) {
       if (!(file instanceof String)) file = file.basename;
       if (this.get(file) === undefined) return false;
       else {
-        const success = delete this[this.get(file)] && delete this.map[file];
+        const success = delete this[this.get(file).toString()] && delete this.map[file];
         if (success) --this.length;
         return success;
       }
