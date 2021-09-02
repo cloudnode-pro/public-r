b63546301b32730c199fd9737ef7c4d859749e5b
williamd5 <will.duncan.nn+github@gmail.com>
2021-09-03 02:03:37 +0300

    we dont need this

--- js/fs.js
+++ js/fs.js
@@ -300,7 +300,6 @@ function FileManager (el, fs = new CloudnodeFS(), options = {}) {
     this.elements.body.querySelector(".filemanager-files").append(d);
 
     // events
-    const fm = this;
     d.addEventListener("dblclick", () => location.hash = `#browse=${file.path}`);
     d.addEventListener("click", (e) => {
       fm.elements.body.querySelectorAll(".filemanager-files .filemanager-file.selected").forEach((e) => e.classList.remove("selected"));
