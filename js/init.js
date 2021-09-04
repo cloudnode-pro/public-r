2c2ad142c763acc1368d9680e46b331496c3490e
williamd5 <will.duncan.nn+github@gmail.com>
2021-09-04 23:39:10 +0300

    replace to fix error

--- js/init.js
+++ js/init.js
@@ -48,7 +48,7 @@ main.init = function (app, el, options = {}) {
 				}
 			}
 		},
-		console: function ($el, options) {
+		console: function (el, options) {
 			main.page.loadScriptOnce("/r/js/mc-colors.js", function () {
 				if (typeof options !== "object") options = {text:"",callbacks:{},label:"$"};
 				if (typeof options.label !== "string") options.label = "$";
@@ -57,25 +57,25 @@ main.init = function (app, el, options = {}) {
 				let lines = options.text.split("\n");
 				let inputHistory = [];
 				for (let i in lines) lines[i] = `<div class="line">${main.MinecraftColorCodes.toHTML(lines[i])}</div>`;
-				$el.html(`<div class="console"><div class="console-view">${lines.join("")}</div><div class="console-input"><div class="input-group"><label class="input-group-text" for="consoleInput">${options.label}</label><input type="text" class="form-control px-0" placeholder="Type a command..." aria-label="${options.label}" id="consoleInput"></div></div></div>`);
-				if (typeof options?.callback === "function") options.callback($el, options);
+				el.html(`<div class="console"><div class="console-view">${lines.join("")}</div><div class="console-input"><div class="input-group"><label class="input-group-text" for="consoleInput">${options.label}</label><input type="text" class="form-control px-0" placeholder="Type a command..." aria-label="${options.label}" id="consoleInput"></div></div></div>`);
+				if (typeof options?.callback === "function") options.callback(el, options);
 			});
 		},
-		dropdownSearch: function ($el) {
-			$el.addClass("dropdown-search");
-			$el.html(`<div class="px-2 pb-2"><input dropdown-search class="form-control form-control-sm form-control-light" palceholder="${main.langData.translate("search...")}"></div>${$el.html()}`);
-			$el.find("[dropdown-search]").input(function () {
+		dropdownSearch: function (el) {
+			el.addClass("dropdown-search");
+			el.html(`<div class="px-2 pb-2"><input dropdown-search class="form-control form-control-sm form-control-light" palceholder="${main.langData.translate("search...")}"></div>${el.html()}`);
+			el.find("[dropdown-search]").input(function () {
 				let v = $(this).val().trim().toLowerCase();
-				$($el[0]).find("li").iterator(function ($li) {
+				$(el[0]).find("li").iterator(function ($li) {
 					if (!$($li).text().toLowerCase().includes(v)) $($li).addClass("d-none");
 					else $($li).removeClass("d-none");
 				});
 			});
-			$($el[0]).parent(".dropdown").on("shown.bs.dropdown", function () {
-				$($el[0]).find("[dropdown-search]").focus().val("").trigger("input");
+			$(el[0]).parent(".dropdown").on("shown.bs.dropdown", function () {
+				$(el[0]).find("[dropdown-search]").focus().val("").trigger("input");
 			});
 		}
 	}
-	if (typeof apps[app] === "function") return apps[app]($el, options);
+	if (typeof apps[app] === "function") return apps[app](el, options);
 	throw new Error(`init.js: enoent application "${app}"`)
 }
